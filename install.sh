2320cea412e71ae2475176035ef864493e1451d7
Cowang4 <cowang4@yahoo.com>
2016-05-02 14:18:12 -0400

    quick update

--- install.sh
+++ install.sh
@@ -3,12 +3,4 @@ DOTFILES_DIR=$(pwd)
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-ln -sfv "$DOTFILES_DIR/.ansiweatherrc" ~
-ln -sfv "$DOTFILES_DIR/.aliases" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/.transfersh" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/.profile" ~
-ln -sfv "$DOTFILES_DIR/.exports" ~
+ln -sfv "$DOTFILES_DIR/.*" ~
