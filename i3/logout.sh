b3eec7f49daecb990061fd604ff4cdd15d3fe313
Greg Cowan <cowang4@yahoo.com>
2017-09-19 15:40:57 -0400

    more cleanup

--- i3/logout.sh
+++ i3/logout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-res=$(rofi -lines 4 -eh 2 -width 100 -padding 450 -opacity "85" -bw 0 -bc "#2f343f" -bg "#2f343f" -fg "#f3f4f5" -hlbg "#2f343f" -hlfg "#9575cd" -font "System San Francisco Display 18" -dmenu < ~/dotfiles/i3/dmenu-i3exit)
+res=$(rofi -lines 4 -eh 2 -width 100 -padding 450 -opacity "85" -bw 0 -bc "#2f343f" -bg "#2f343f" -fg "#f3f4f5" -hlbg "#2f343f" -hlfg "#9575cd" -font "Hack Regular 18" -dmenu < ~/dotfiles/i3/dmenu-i3exit)
 
 if [ $res = "lock" ]; then
   i3lock -i ~/Pictures/Wallpapers/everest.png
