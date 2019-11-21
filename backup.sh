c3920e4024c295fcf9366fe9e929cfda5a9bb7f4
Greg Cowan <cowang4@gmail.com>
2019-11-20 21:09:33 -0800

    fix backup script

--- backup.sh
+++ backup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-rsync -rlPvv $HOME/.ssh pi@shock:/media/1TB_Drive/Backups/volt
-rsync -rlPvv $HOME/.bpb_keys.toml pi@shock:/media/1TB_Drive/Backups/volt/.bpb_keys.toml
-rsync -rlPvv $HOME/Pictures pi@shock:/media/1TB_Drive/Backups/volt
-rsync -rlPvv $HOME/Documents pi@shock:/media/1TB_Drive/Backups/volt \
-  --exclude $HOME/Documents/SideProjects/my-mycroft \
-  --exclude $HOME/Documents/SideProjects/new-site/target \
-  --exclude $HOME/Documents/School/Thesis/dht/target
+restic backup $HOME/Pictures
+restic backup $HOME/Documents
